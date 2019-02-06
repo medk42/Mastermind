1e9f6e8707f90fab7be4ad849fd6a28ebcb6f71d
Jakub Medek <medekjak@gmail.com>
2019-02-07 00:53:03 +0100

    Add State class, Mastermind constructor and initializer

--- Mastermind.java
+++ Mastermind.java
@@ -1,14 +1,45 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Mastermind {
+    private ArrayList<State> possibilities;
+    private int positionCount, colorCount;
+
+    public Mastermind(int positionCount, int colorCount) {
+        possibilities = new ArrayList<State>();
+        this.positionCount = positionCount;
+        this.colorCount = colorCount;
+
+        initializePossibilities(positionCount-1, colorCount, new int[positionCount]);
+    }
+
+    private void initializePossibilities(int positionIndex, int colorCount, int[] positions) {
+        if (positionIndex < 0) {
+            possibilities.add(new State(positions));
+            return;
+        }
+        for (int color = 0; color < colorCount; color++) {
+            positions[positionIndex] = color;
+            initializePossibilities(positionIndex-1, colorCount, positions);
+        }
+    }
+
+    public int getPositionCount() {
+        return positionCount;
+    }
+
+    public int getColorCount() {
+        return colorCount;
+    }
+
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
 
         int positionCount = getPositiveInteger(scanner, "Enter position count: ");
         int colorCount = getPositiveInteger(scanner, "Enter color count: ");
 
-        System.out.printf("Position count: %d    Color count: %d\n", positionCount, colorCount);
-
+        Mastermind game = new Mastermind(positionCount, colorCount);
+        
         scanner.close();
     }
 
@@ -25,5 +56,22 @@ class Mastermind {
             }
         }
         return retVal;
+    }
+
+    class State {
+        private int[] positions;
+
+        public State(int positionCount) {
+            positions = new int[positionCount];
+        }
+
+        public State(int[] values) {
+            this(values.length);
+            for (int i = 0; i < positions.length; i++) positions[i] = values[i];
+        }
+
+        public int getPositionValue(int index) {
+            return positions[index];
+        }
     }
 }
