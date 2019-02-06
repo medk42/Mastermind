9cf848d6a28ba5e11a0a2fc9ec48bb0bdd3fff64
Jakub Medek <medekjak@gmail.com>
2019-02-07 00:09:34 +0100

    Add getPositiveInteger function
    Used for getting number of colors and positions

--- Mastermind.java
+++ Mastermind.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 class Mastermind {
     public static void main(String args[]) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        int positionCount = getPositiveInteger(scanner, "Enter position count: ");
+        int colorCount = getPositiveInteger(scanner, "Enter color count: ");
+
+        System.out.printf("Position count: %d    Color count: %d\n", positionCount, colorCount);
+
+        scanner.close();
+    }
+
+    public static int getPositiveInteger(Scanner scanner, String message) {
+        System.out.print(message);
+        int retVal = -1;
+        while (retVal <= 0) {
+            try {
+                retVal = Integer.parseInt(scanner.nextLine());
+            } catch(Exception e) {}
+            if (retVal <= 0) {
+                System.out.println("Please enter postive integer!");
+                System.out.print(message);
+            }
+        }
+        return retVal;
     }
 }
